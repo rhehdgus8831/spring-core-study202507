d0ee4589360fa5534e45b8aa36ad8ddd17dfc753
rhehdgus8831 <rhehdgus8831@naver.com>
2025-07-17 11:45:32 +0900

    Initial commit

---
+++ src/main/java/com/spring/core/SpringCor202507Application.java
@@ -0,0 +1,13 @@
+package com.spring.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCor202507Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCor202507Application.class, args);
+    }
+
+}
